1ec4138e07a96408082f54d86525da1a376a9143
ZhoraBoom <chirkov.04@mail.ru>
2021-04-06 12:04:20 +0300

    Update test.cpp

--- complex/test.cpp
+++ complex/test.cpp
@@ -1,154 +1,160 @@
 #include <iostream>
-#include <cmath>
+#include <limits>
+#include <sstream>
+
 using namespace std;
-class Complex {
-public:
-    Complex() {
-    }
 
-    Complex(const double real, const double imaginary) {
-        _real = real;
-        _imaginary = imaginary;
-    }
-
-    Complex(const Complex& copied) {
-        _real = copied._real;
-        _imaginary = copied._imaginary;
-    }
-
-    Complex& operator=(const Complex& copied) {
-        _real = copied._real;
-        _imaginary = copied._imaginary;
-    }
-
-    ~Complex() {
-    }
-
-    bool operator!=(const Complex& other) const {
-        if(_real != other._real || _imaginary != other._imaginary){
-            return true;
-        }
-        return false;
-    }
-
-    bool operator==(const Complex& other) const {
-        if(_real == other._real && _imaginary == other._imaginary){
-            return true;
-        }
-        return false;
-    }
-
-    Complex operator-() const {
-        Complex answer(* this);
-        answer._real = (-1)* _real;
-        answer._imaginary = (-1) * _imaginary;
-        return answer;
-    }
-
-    Complex operator+(const Complex& other) const {
-        Complex answer(* this);
-        answer._real += other._real;
-        answer._imaginary += other._imaginary;
-        return answer;
-    }
-
-    Complex operator-(const Complex& other) const {
-        Complex answer(* this);
-        answer._real = answer._real - other._real;
-        answer._imaginary = answer._imaginary - other._imaginary;
-        return answer;
-    }
-
-    Complex operator*(const Complex& other) const {
-        Complex answer(* this);
-        answer._real = _real * other._real - _imaginary * other._imaginary;
-        answer._imaginary = _real * other._imaginary + _imaginary * other._real;
-        return answer;
-    }
-
-    Complex operator/(const Complex& other) const {
-        Complex answer(* this);
-        answer._real = (_real * other._real + _imaginary * other._imaginary) / (other._real * other._real + other._imaginary * other._imaginary);
-        answer._imaginary = (_imaginary * other._real - _real * other._imaginary) / (other._real * other._real + other._imaginary * other._imaginary);
-        return answer;
-    }
-
-    double module() const {
-        return sqrt(_real * _real + _imaginary * _imaginary);
-    }
-
-    double argument() const {
-        return atan2(_real, _imaginary);
-    }
-
-    Complex Conjugate() const {
-        Complex answer(* this);
-        answer._real = _real;
-        answer._imaginary = (-1) * _imaginary;
-        return answer;
-    }
-
-private:
-    double _real, _imaginary;
+class BigInt {
 };
 
-int main() {
-    int errors = 0, tests = 0;
-    Complex a;
-    Complex b(1, 2);
-    Complex c(b);
-    ++tests;
-    if (b != c) {
-        cout << "Error: b != c" << endl;
-        ++errors;
-    }
-    Complex d(2, 3);
-    ++tests;
-    if (b == d) {
-        cout << "Error: b == d" << endl;
-        ++errors;
-    }
-    Complex e = d;
-    ++tests;
-    if (d != e) {
-        cout << "Error: d != e" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (Complex(3, 4).module() != 5) {
-        cout << "Error: |3 + 4i| != 5" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (fabs(Complex(3, 4).argument() - 0.6435011087) > 0.0000001) {
-        cout << "Error: arg(3 + 4i) != " << endl;
-        ++errors;
-    }
-    ++tests;
-    if (- Complex(1, -3) != Complex(-1, 3)) {
-        cout << "Error: - Complex(1, -3) != Complex(-1, 3)" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (Complex(1, -3) + Complex(4, 2) != Complex(5, -1)) {
-        cout << "Error: Complex(1, -3) + Complex(4, 2) != Complex(5, -1)" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (Complex(1, -3) - Complex(4, 2) != Complex(-3, -5)) {
-        cout << "Error: Complex(1, -3) - Complex(4, 2) != Complex(-3, -5)" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (Complex(4, 3) * Complex(1, -2) != Complex(10, -5)) {
-        cout << "Error: Complex(4, 3) * Complex(1, -2) != Complex(10, -5)" << endl;
-        ++errors;
-    }
-    ++tests;
-    if (Complex(4, 3) / Complex(1, 2) != Complex(2, -1)) {
-        cout << "Error: Complex(4, 3) / Complex(1, 2) != Complex(2, -1)" << endl;
-        ++errors;
-    }
-    cout << "Number of tests: " << tests << ", number of errors: " << errors << endl;
-    return 0;
+std::string toString(const BigInt& value)
+{
+    std::stringstream buf;
+    buf << value;
+    return buf.str();
 }
+
+void check(int64_t x, int64_t y)
+{
+    const BigInt bigX = x;
+    const BigInt bigY = y;
+
+    if (bigX + bigY != BigInt(x + y))
+    {
+        std::cout << x << " + " << y << " != " << x + y << " got " << bigX + bigY << '\n';
+    }
+
+    if (bigX - bigY != BigInt(x - y))
+    {
+        std::cout << x << " - " << y << " != " << x - y << " got " << bigX - bigY << '\n';
+    }
+
+    if (bigX * bigY != BigInt(x * y))
+    {
+        std::cout << x << " * " << y << " != " << x * y << " got " << bigX * bigY << '\n';
+    }
+
+    if (x != 0 && y != 0)
+    {
+        if (bigX / bigY != BigInt(x / y))
+        {
+            std::cout << x << " / " << y << " != " << x / y << " got " << bigX / bigY << '\n';
+        }
+        if (bigX % bigY != BigInt(x % y))
+        {
+            std::cout << x << " % " << y << " != " << x % y << " got " << bigX % bigY << '\n';
+        }
+    }
+}
+
+void doCheckEqual(const BigInt& actual, const char* expected, size_t line)
+{
+    const auto str = toString(actual);
+    if (str != expected)
+    {
+        std::cout << "at line " << line << ": " << str << " != " << expected << '\n';
+    }
+}
+
+#define checkEqual(x, y) do { doCheckEqual((x), (y), __LINE__); } while(0)
+#define checkTrue(cond) do { if (!(cond)) std::cout << "at line " << __LINE__ << ": " << #cond << '\n'; } while(0)
+
+int main()
+{
+    BigInt x = 3;
+    checkEqual(x, "3");
+    BigInt y = x;
+    checkEqual(y, "3");
+    BigInt z;
+    checkEqual(z, "0");
+
+    checkEqual(BigInt(-10), "-10");
+
+    checkTrue(x == y);
+    checkTrue(y == x);
+    checkTrue(x != z);
+    checkTrue(z != x);
+
+    z = y;
+    checkEqual(z, "3");
+
+    x = 100;
+    checkEqual(x, "100");
+
+    checkTrue(!(x < x));
+    checkTrue(x < 200);
+    checkTrue(BigInt(50) < x);
+    checkTrue(BigInt(-500) < x);
+    checkTrue(BigInt(-500) < BigInt(-200));
+
+    checkTrue(!(x > x));
+    checkTrue(BigInt(200) > x);
+    checkTrue(x > BigInt(50));
+    checkTrue(x > BigInt(-500));
+    checkTrue(BigInt(-200) > BigInt(-500));
+
+    checkTrue(x <= x);
+    checkTrue(x <= 200);
+    checkTrue(BigInt(50) <= x);
+    checkTrue(BigInt(-500) <= x);
+    checkTrue(BigInt(-500) <= BigInt(-200));
+
+    checkTrue(x >= x);
+    checkTrue(BigInt(200) >= x);
+    checkTrue(x >= BigInt(50));
+    checkTrue(x >= BigInt(-500));
+    checkTrue(BigInt(-200) >= BigInt(-500));
+    checkTrue(BigInt(0) == -BigInt(0));
+
+    checkEqual(BigInt(10) + BigInt(10), "20");
+    checkEqual(BigInt(-10) + BigInt(10), "0");
+    checkEqual(BigInt(10) + BigInt(-10), "0");
+    checkEqual(BigInt(-10) + BigInt(-10), "-20");
+
+    checkEqual(BigInt(10) - BigInt(10), "0");
+    checkEqual(BigInt(-10) - BigInt(10), "-20");
+    checkEqual(BigInt(10) - BigInt(-10), "20");
+    checkEqual(BigInt(-10) - BigInt(-10), "0");
+
+    checkEqual(BigInt(0) + BigInt(-1), "-1");
+    checkEqual(BigInt(0) - BigInt(1), "-1");
+
+    checkEqual(BigInt(100) - BigInt(100), "0");
+    checkEqual(BigInt(99) - BigInt(100), "-1");
+    checkEqual(BigInt(10) - BigInt(11), "-1");
+    checkEqual(BigInt(20) - BigInt(19), "1");
+
+    for (int i = -21; i <= 21; ++i)
+    {
+        for (int j = -21; j <= 21; ++j)
+        {
+            check(i, j);
+        }
+    }
+
+    const int64_t step = std::numeric_limits<uint32_t>::max() / 99;
+    const int64_t lower = std::numeric_limits<int32_t>::min() + step;
+    const int64_t upper = std::numeric_limits<int32_t>::max() - step;
+
+    for (int64_t i = lower; i < upper; i += step)
+    {
+        for (int64_t j = -99; j < 99; ++j)
+        {
+            check(i, j);
+        }
+    }
+
+    const BigInt big1 = std::numeric_limits<int64_t>::max();
+    checkEqual(big1, "9223372036854775807");
+
+    const BigInt big2 = big1 * big1;
+    std::cout << "9223372036854775807 * 9223372036854775807\n";
+    checkEqual(big2, "85070591730234615847396907784232501249");
+
+    const BigInt big3 = big2 * big2;
+    std::cout << "85070591730234615847396907784232501249 * 85070591730234615847396907784232501249\n";
+    checkEqual(big3, "7237005577332262210834635695349653859421902880380109739573089701262786560001");
+
+    std::cout << "done\n";
+
+    return 0;
