4e89ef8eb9acb0b32e2b0d6682c5fe2d025d1d91
ZhoraBoom <chirkov.04@mail.ru>
2021-04-06 11:45:37 +0300

    update\.test.cpp

--- complex/test.cpp
+++ complex/test.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-
 class Complex {
 public:
     Complex() {
     }
 
     Complex(const double real, const double imaginary) {
-        real = _real;
-        imaginary = _imaginary;
+        _real = real;
+        _imaginary = imaginary;
     }
 
     Complex(const Complex& copied) {
@@ -41,43 +39,38 @@ public:
     }
 
     Complex operator-() const {
-        Complex answer--(){
-            answer--._real = (-1)* _real;
-            answer--._imaginary = (-1) * _imaginary;
-            return answer--;
-        }
+        Complex answer(* this);
+        answer._real = (-1)* _real;
+        answer._imaginary = (-1) * _imaginary;
+        return answer;
     }
 
     Complex operator+(const Complex& other) const {
-        Complex answer+(* this) {
-            answer+._real += other._real;
-            answer+._imaginary += other._imaginary;
-            return answer+;
-        }
+        Complex answer(* this);
+        answer._real += other._real;
+        answer._imaginary += other._imaginary;
+        return answer;
     }
 
     Complex operator-(const Complex& other) const {
-        Complex answer-(* this) {
-            answer-._real = answer-._real - other._real;
-            answer-._imaginary = answer-._imaginary - other._imaginary;
-            return answer-;
-        }
+        Complex answer(* this);
+        answer._real = answer._real - other._real;
+        answer._imaginary = answer._imaginary - other._imaginary;
+        return answer;
     }
 
     Complex operator*(const Complex& other) const {
-        Complex answer*(* this){
-            answer*._real = _real * other._real - _imaginary * other._imaginary;
-            answer*._imaginary = _real * other._imaginary + _imaginary * other._real;
-            return answer*;
-        }
+        Complex answer(* this);
+        answer._real = _real * other._real - _imaginary * other._imaginary;
+        answer._imaginary = _real * other._imaginary + _imaginary * other._real;
+        return answer;
     }
 
     Complex operator/(const Complex& other) const {
-        Complex answer/ (* this){
-            answer/._real = (_real * other._real + _imaginary * other._imaginary) / (other._real * other._real + other._ imaginary * other._imaginary);
-            answer/._imaginary = (_imaginary * other._real - _real * other._imaginary) / (other._real * other._real + other._ imaginary * other._imaginary);
-            return answer/;
-        }
+        Complex answer(* this);
+        answer._real = (_real * other._real + _imaginary * other._imaginary) / (other._real * other._real + other._imaginary * other._imaginary);
+        answer._imaginary = (_imaginary * other._real - _real * other._imaginary) / (other._real * other._real + other._imaginary * other._imaginary);
+        return answer;
     }
 
     double module() const {
@@ -89,27 +82,19 @@ public:
     }
 
     Complex Conjugate() const {
-        Complex anscon(){
-            anscon._real = _real;
-            anscon._imaginary = (-1) * _imaginary;
-            return anscon;
-        }
+        Complex answer(* this);
+        answer._real = _real;
+        answer._imaginary = (-1) * _imaginary;
+        return answer;
     }
 
 private:
     double _real, _imaginary;
-    void answer--();
-    void answer-();
-    void answer+();
-    void answer*();
-    void answer/();
-    void anscon();
 };
 
 int main() {
     int errors = 0, tests = 0;
     Complex a;
-
     Complex b(1, 2);
     Complex c(b);
     ++tests;
@@ -117,7 +102,6 @@ int main() {
         cout << "Error: b != c" << endl;
         ++errors;
     }
-
     Complex d(2, 3);
     ++tests;
     if (b == d) {
@@ -130,50 +114,41 @@ int main() {
         cout << "Error: d != e" << endl;
         ++errors;
     }
-
     ++tests;
     if (Complex(3, 4).module() != 5) {
         cout << "Error: |3 + 4i| != 5" << endl;
         ++errors;
     }
-
     ++tests;
     if (fabs(Complex(3, 4).argument() - 0.6435011087) > 0.0000001) {
         cout << "Error: arg(3 + 4i) != " << endl;
         ++errors;
     }
-
     ++tests;
     if (- Complex(1, -3) != Complex(-1, 3)) {
         cout << "Error: - Complex(1, -3) != Complex(-1, 3)" << endl;
         ++errors;
     }
-
     ++tests;
     if (Complex(1, -3) + Complex(4, 2) != Complex(5, -1)) {
         cout << "Error: Complex(1, -3) + Complex(4, 2) != Complex(5, -1)" << endl;
         ++errors;
     }
-
     ++tests;
     if (Complex(1, -3) - Complex(4, 2) != Complex(-3, -5)) {
         cout << "Error: Complex(1, -3) - Complex(4, 2) != Complex(-3, -5)" << endl;
         ++errors;
     }
-
     ++tests;
     if (Complex(4, 3) * Complex(1, -2) != Complex(10, -5)) {
         cout << "Error: Complex(4, 3) * Complex(1, -2) != Complex(10, -5)" << endl;
         ++errors;
     }
-
     ++tests;
     if (Complex(4, 3) / Complex(1, 2) != Complex(2, -1)) {
         cout << "Error: Complex(4, 3) / Complex(1, 2) != Complex(2, -1)" << endl;
         ++errors;
     }
-
     cout << "Number of tests: " << tests << ", number of errors: " << errors << endl;
-
     return 0;
 }
